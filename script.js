0a534294639df521028761827c86a16ac3c6def9
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-05 13:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.flex');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('.volume');
+const playbackSpeed = document.querySelector('.playbackSpeed');
+const progressFilled = document.querySelector('.progress__filled');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+// 1. Play / Pause
+toggle.addEventListener('click', function () {
+
+    if (video.paused) {
+        video.play();
+        toggle.textContent = '❚ ❚';
+    } else {
+        video.pause();
+        toggle.textContent = '►';
     }
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+});
+
+
+// 2. Volume
+volume.addEventListener('input', function () {
+
+    video.volume = Number(this.value);
+
+});
+
+
+// 3. Playback Speed
+playbackSpeed.addEventListener('input', function () {
+
+    video.playbackRate = Number(this.value);
+
+});
+
+
+// 4. Rewind / Forward
+skipButtons.forEach(function (button) {
+
+    button.addEventListener('click', function () {
+
+        video.currentTime += Number(this.dataset.skip);
+
+    });
+
+});
+
+
+// 5. Progress Bar
+video.addEventListener('timeupdate', function () {
+
+    const percent = (video.currentTime / video.duration) * 100;
+
+    progressFilled.style.width = `${percent}%`;
+
+});
